aca64657c453522913dd410d9126729e5f3d86bb
J-Ahmad19 <jawwadahmad568@gmail.com>
2025-08-18 02:00:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,13 +2,9 @@ import requests
 import streamlit as st
 import json
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-api_key = os.getenv("OPENROUTER_API_KEY")
 
 
+api_key=st.sectrets["OPENROUTER_API_KEY"]
 
 
 
